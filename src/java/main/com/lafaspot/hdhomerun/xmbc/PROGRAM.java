d247f5d1a45c1cbf5d7340f81acf55a1fe1da99d
lafaspot <lafaspot@9d562b8c-5cf6-8d0a-f0b4-ddb9a495a75c>
2010-07-08 16:26:17 +0000

    fix bug when unable to parser PROGRAM line on the scan FILE

--- src/java/main/com/lafaspot/hdhomerun/xmbc/PROGRAM.java
+++ src/java/main/com/lafaspot/hdhomerun/xmbc/PROGRAM.java
@@ -15,6 +15,10 @@ class PROGRAM extends SCANOBJ {
 
 	public String description() {		
 		int pos = data.indexOf(":");
+		if (pos == -1) {
+			System.err.println("PROGRAM.description missing from line: " + data);
+			return "NOT FOUND";
+		}
 		String description = data.substring(pos+2);
 		if (description.trim().equals("0") || description.trim().equals("")) {
 			description = mParent.channel("us-cable") + "-" + number();
@@ -23,12 +27,17 @@ class PROGRAM extends SCANOBJ {
 	}
 
 	public String number() {
-		int pos = data.indexOf(":");		
+		int pos = data.indexOf(":");
+		if (pos == -1) {
+			System.err.println("PROGRAM.number missing from line: " + data);
+			return "NOT FOUND";
+		}
 		return data.substring(8, pos);
 	}
 
 	public boolean isDescriptionAvailable() {
 		int pos = data.indexOf(":");
+		if (pos == -1) return false;
 		String description = data.substring(pos+2);
 		if (description.trim().equals("0") || description.trim().equals("")
 		    || description.trim().equals("0 (control)")|| description.trim().equals("0 (no data)")) {
